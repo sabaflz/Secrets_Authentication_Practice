5ab8a278f481689553866b553d53bf1263b8692f
Saba <saba.feilizadeh@gmail.com>
2022-02-11 21:03:06 -0800

    Encrypt database. Security level 2.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -16,10 +17,15 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB" , {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-}
+});
+// encryption
+//only encrypt certain fields
+const secret = "Thisisourlittlesecret!";
+userSchema.plugin(encrypt, {secret: secret , encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
