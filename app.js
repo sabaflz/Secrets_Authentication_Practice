893007e42be8fcdd329622bd5b7be07fe1e37a2e
Saba <saba.feilizadeh@gmail.com>
2022-02-11 21:31:19 -0800

    Add level 3 security. Add Hash function to encrypt passwords.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 // encryption
 //only encrypt certain fields
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -45,7 +46,7 @@ app.post("/register" , function(req , res){
   const newUser = new User(
     {
       email: req.body.username ,
-      password: req.body.password
+      password: md5(req.body.password)
     });
 
     newUser.save(function(err){
